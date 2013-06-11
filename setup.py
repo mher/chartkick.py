1a7c1a2ed2d0a54f37bf04e21ccf94a5134346cc
Mher Movsisyan <mher.movsisyan@gmail.com>
2013-06-11 15:42:58 +0400

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+version = re.compile(r'VERSION\s*=\s*\((.*?)\)')
+
+
+def get_package_version():
+    "returns package version without importing it"
+    base = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(base, "chartkick/__init__.py")) as initf:
+        for line in initf:
+            m = version.match(line.strip())
+            if not m:
+                continue
+            return ".".join(m.groups()[0].split(", "))
+
+
+classes = """
+    Development Status :: 3 - Alpha
+    Intended Audience :: Developers
+    License :: OSI Approved :: MIT License
+    Topic :: Charts :: Google Charts :: Highcharts
+    Programming Language :: Python
+    Programming Language :: Python :: 2
+    Programming Language :: Python :: 2.6
+    Programming Language :: Python :: 2.7
+    Programming Language :: Python :: Implementation :: CPython
+    Operating System :: OS Independent
+"""
+classifiers = [s.strip() for s in classes.split('\n') if s]
+
+
+setup(
+    name='chartkick',
+    version=get_package_version(),
+    description='Create beautiful Javascript charts with minimal code',
+    long_description=open('README.rst').read(),
+    author='Mher Movsisyan',
+    author_email='mher.movsisyan@gmail.com',
+    url='https://github.com/mher/chartkick.py',
+    license='MIT',
+    classifiers=classifiers,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    package_data={'chartkick': ['js/*']},
+)
