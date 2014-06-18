cb4c147bcaf4378bcd9a7ce79c921b4e996cb16c
Mher Movsisyan <mher.movsisyan@gmail.com>
2014-06-18 15:26:49 +0400

    Add Python3 support

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ classes = """
     Programming Language :: Python :: 2
     Programming Language :: Python :: 2.6
     Programming Language :: Python :: 2.7
+    Programming Language :: Python :: 3.3
+    Programming Language :: Python :: 3.4
     Programming Language :: Python :: Implementation :: CPython
     Operating System :: OS Independent
 """
